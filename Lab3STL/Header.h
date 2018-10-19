4932999908ba1f8e960bf8228f8f267ea95bc854
Anton <antourv@gmail.com>
2018-10-19 19:34:51 +0700

    Добавьте файлы проекта.

---
+++ Lab3STL/Header.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <iostream>
+#include <iterator>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <type_traits>
+#include <algorithm>
+
+#define Strings std::vector <std::string>
+
+bool compare(std::string const& a, std::string const& b)
+{
+	return a.size() < b.size();
+}
+
+void CompareOfString()
+{
+	Strings String; 
+	std::map<char, Strings> Text;
+	try 
+	{
+		std::ifstream fin("input.txt"); 
+		if (!fin.is_open()) 
+			std::cout << "Error while opening the file\n"; 
+		else
+		{
+			std::string symb;
+			while (std::getline(fin, symb))
+			{
+				Text[tolower(symb[0])].insert(Text[tolower(symb[0])].end(), symb);
+			}
+			symb.clear();
+			fin.close();
+			std::ofstream fout("output.txt");
+			for (auto it = Text.begin(); it != Text.end(); it++)
+			{
+				std::sort((*it).second.begin(), (*it).second.end());
+				for (int i = 0; i < (*it).second.size(); i++) 
+				{
+					fout << (*it).second[i] << std::endl;
+				}
+			}
+			fout.close();
+		}
+	}
+	catch (std::bad_alloc) {
+		std::cout << "Could not allocate memory";
+	}
+}
